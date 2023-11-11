158c5f94e40a5f7ee3d392896e43f1dfeaf1f2f1
Lucy Lennemann <llennema@ucsd.edu>
2023-11-11 13:15:02 -0800

    Added route for getting transcript from video, javascript to display transcript + video

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # VideoChatBot
 
-If you want to use a virtual environment, run the command 'venv\Scripts\activate' 
+If you want to use the virtual environment, run the command 'venv\Scripts\activate' 
 on Windows and 'source venv/bin/activate' on Mac. 
+
+pip install -r requirements.txt -> set up project environment on your computer
 
 Test the frontend by running the command, python app.py
 Then navigate to 'http://127.0.0.1:5000/' in the web browser.
---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('uploadForm').addEventListener('submit', function(e) {
+        // When the form is submitted, prevents the default form submission and 
+        // instead sends the video file to the Flask server using fetch.
+        e.preventDefault();
+
+        // create formData object:  create a set of key/value pairs representing 
+        // form fields and their values, which includes the uploaded file
+        const formData = new FormData();
+        const videoFile = document.querySelector('input[type="file"]').files[0];
+        formData.append('video', videoFile);
+
+        print(formData);
+
+        fetch('/upload-video', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Display the video
+            const videoPlayer = document.getElementById('videoPlayer');
+            videoPlayer.style.display = 'block';
+            videoPlayer.src = URL.createObjectURL(videoFile);
+
+            // Display the transcript
+            const transcriptDiv = document.getElementById('transcript');
+            console.log(data);
+            console.log(data.transcript['text'])
+            transcriptDiv.textContent = data.transcript['text'];
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
