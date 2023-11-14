ba44dff8298af6baafdabc42138aefd78b56dfc8
Greencouch82 <100258636+Greencouch82@users.noreply.github.com>
2023-11-13 16:23:00 -0800

    basic chatbot file

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+
+# Initialize the OpenAI client with your API key
+client = OpenAI(api_key=api_key)
+
+def chat_with_bot(prompt_text):
+    response = client.chat.completions.create(
+        model="gpt-4",  # Replace with your specific GPT-4 model identifier
+        messages=[
+            {"role": "user", "content": prompt_text}
+        ]
+    )
+    # Accessing the content from the response's choices attribute
+    message = response.choices[0].message.content
+    return message
+
+
+
+def main():
+    print("Chatbot initialized. You can start chatting with me! Type 'quit' to exit.")
+    
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "quit":
+            print("Chatbot exiting. Goodbye!")
+            break
+        bot_response = chat_with_bot(user_input)
+        print("Bot:", bot_response)
+
+if __name__ == "__main__":
+    main()
