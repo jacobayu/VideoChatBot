f53176969da8456c7b8a140d7462043a47a633dc
Jacob Au <jacobau.inbox@gmail.com>
2023-11-14 18:17:00 -0800

    stylized video and transcript

--- transcript.py
+++ transcript.py
@@ -24,6 +24,21 @@ def transcribe(video_path, audio_path, transcript_path):
     # Return the transcription text
     return result
 
-# extract_audio('static/NeuralNetVideo.mp4', 'static/NeuralNetAudio.wav')
-# transcript = transcribe_audio('static/NeuralNetVideo.mp4', static/NeuralNetAudio.wav', 'static/transcript.txt')
+
+def transcribe_audio(audio_path, transcript_path):
+
+    # Load the Whisper model
+    model = whisper.load_model("base")
+
+    # Process the audio file and generate the transcription
+    result = model.transcribe(audio_path)
+
+    with open(transcript_path, 'w', encoding='utf-8') as file:
+        json.dump(result, file, ensure_ascii=False, indent=4)
+
+    # Return the transcription text
+    return result
+
+# extract_audio('Amazon Filters.mp4', 'audio-files/VideoAudio.wav')
+# transcript = transcribe_audio('audio-files/VideoAudio.wav', 'audio-files/VidTranscript.txt')
 # print(transcript)
