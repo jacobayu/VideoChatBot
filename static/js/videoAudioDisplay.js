8f85bdab5eef66cf495bb20359baea8b57e8c375
Jacob Au <jacobau.inbox@gmail.com>
2023-11-14 18:17:00 -0800

    stylized video and transcript

--- static/js/videoAudioDisplay.js
+++ static/js/videoAudioDisplay.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-   
-    
     fetch('/audio-files/VidTranscript.json')
     .then(response => response.json()) // Parse the JSON string in the response
     .then(data => {
@@ -12,32 +10,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const videoPlayer = document.getElementById('videoPlayer');
         const transcriptContainer = document.getElementById('transcript');
         const transcriptData = data;
+        const container = document.getElementById('transcript-container');
+        const MIN_LENGTH = 30; 
+
         videoPlayer.style.display = 'block';
+        let currentSpan = null;
 
         // Generate transcript HTML from JSON data
         transcriptData.segments.forEach(segment => {
-            let span = document.createElement('span');
-            span.classList.add('transcript-segment');
-            span.setAttribute('data-start', segment.start);
-            span.setAttribute('data-end', segment.end);
-            span.textContent = segment.text + ' ';
-            transcriptContainer.appendChild(span);
+            let textLength = segment.text.trim().length;
+
+            if (!currentSpan || textLength >= MIN_LENGTH) {
+                currentSpan = document.createElement('span');
+                currentSpan.classList.add('transcript-segment');
+                currentSpan.textContent = segment.text + ' ';
+                transcriptContainer.appendChild(currentSpan);
+            } else {
+                currentSpan.textContent += segment.text + ' ';
+            }
+
+            currentSpan.setAttribute('data-start', segment.start);
+            currentSpan.setAttribute('data-end', segment.end);
         });
 
         const segments = document.getElementsByClassName('transcript-segment');
 
         videoPlayer.addEventListener('timeupdate', function() {
             let currentTime = videoPlayer.currentTime;
-
+            let activeSegment = null;
+    
             for (let segment of segments) {
                 let start = parseFloat(segment.getAttribute('data-start'));
                 let end = parseFloat(segment.getAttribute('data-end'));
-
+    
                 if (currentTime >= start && currentTime <= end) {
-                    segment.style.backgroundColor = 'yellow'; // Highlight the active segment
+                    segment.classList.add('active-segment');
+                    activeSegment = segment;
                 } else {
-                    segment.style.backgroundColor = ''; // Remove highlight from inactive segments
+                    segment.classList.remove('active-segment');
                 }
+            }
+    
+            // Auto-scroll within the transcript container
+            if (activeSegment) {
+                let activeSegmentTop = activeSegment.offsetTop;
+                let containerHalfHeight = container.offsetHeight / 2;
+                container.scrollTop = activeSegmentTop - containerHalfHeight + (activeSegment.offsetHeight / 2);
             }
         });
     })    
